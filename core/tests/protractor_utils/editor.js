d21af5759bc7cb88b5ef6309946d53b59f595947
MAKOSCAFEE <barnabasmakonda@gmail.com>
2016-04-07 21:37:55 +0300

    fix

--- core/tests/protractor_utils/editor.js
+++ core/tests/protractor_utils/editor.js
@@ -64,9 +64,8 @@ var finishTutorial = function() {
 var navigateToMainTab = function() {
   element(by.css('.protractor-test-main-tab')).click();
   // Click a neutral element in order to dismiss any warnings.
-  WebElement targetElement = driver.findElement(
-    by.css('.protractor-test-editor-neutral-element'));
-  browser.executeScript('arguments[0].click()', targetElement); 
+  browser.executeScript('arguments[0].click()',
+    element(by.css('.protractor-test-editor-neutral-element'))); 
   //element(by.css('.protractor-test-editor-neutral-element')).click();
 };
 
