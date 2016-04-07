beb40c24a771099b9a9389c6d3479ad0c9c483c3
MAKOSCAFEE <barnabasmakonda@gmail.com>
2016-04-07 22:16:26 +0300

    opps i forgot

--- core/tests/protractor.conf.js
+++ core/tests/protractor.conf.js
@@ -78,7 +78,7 @@ var config = {
     // Set a wide enough window size for the navbar in the gallery to display
     // fully.
     //browser.driver.manage().window().setSize(1200, 1000);
-    driver.manage().window().maximize();
+    browser.driver.manage().window().maximize();
   },
 
   // The params object will be passed directly to the protractor instance,
