dff105d6d26ef4542a043d5d90bcef96cf1dd31c
MAKOSCAFEE <barnabasmakonda@gmail.com>
2016-04-07 14:03:34 +0300

    I think is perfect

--- core/tests/protractor.conf.js
+++ core/tests/protractor.conf.js
@@ -2,6 +2,7 @@ var ScreenShotReporter = require('protractor-screenshot-reporter');
 
 // A reference configuration file.
 var config = {
+  
 
   // ----- What tests to run -----
   //
@@ -30,7 +31,7 @@ var config = {
   //
   // A base URL for your application under test. Calls to protractor.get()
   // with relative paths will be prepended with this.
-  baseUrl: 'http://localhost:9001',
+  baseUrl: 'http://localhost:4445',
 
   // Selector for the element housing the angular app - this defaults to
   // body, but is necessary if ng-app is on a descendant of <body>
@@ -144,10 +145,12 @@ if (process.env.TRAVIS) {
   config.sauceUser = process.env.SAUCE_USERNAME;
   config.sauceKey = process.env.SAUCE_ACCESS_KEY;
   config.capabilities = {
-    browserName: 'chrome', 
-    chromedriverVersion: '2.20',
+    'name': 'oppia',
+    'browserName': 'chrome',
+    'seleniumVersion': '2.48.2',
+    'chromedriverVersion': '2.20',
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-    build: process.env.TRAVIS_BUILD_NUMBER
+    'build': process.env.TRAVIS_BUILD_NUMBER
   };
 } else {
   // ----- How to setup Selenium -----
@@ -168,35 +171,36 @@ if (process.env.TRAVIS) {
   // to the location of this config. If no other method of starting selenium
   // is found, this will default to
   // node_modules/protractor/selenium/selenium-server...
-  seleniumServerJar: null,
+  config.seleniumServerJar = null;
   // The port to start the selenium server on, or null if the server should
   // find its own unused port.
-  seleniumPort: null,
+  config.seleniumPort = null;
   // Chromedriver location is used to help the selenium standalone server
   // find chromedriver. This will be passed to the selenium jar as
   // the system property webdriver.chrome.driver. If null, selenium will
   // attempt to find chromedriver using PATH.
-  chromeDriver: './selenium/chromedriver',
+  config.chromeDriver = './selenium/chromedriver';
   // If true, only chromedriver will be started, not a standalone selenium.
   // Tests for browsers other than chrome will not run.
-  chromeOnly: false,
+  config.chromeOnly = false;
   // Additional command line options to pass to selenium. For example,
   // if you need to change the browser timeout, use
   // seleniumArgs: ['-browserTimeout=60'],
-  seleniumArgs: [],
+  config.seleniumArgs = [];
 
   // If sauceUser and sauceKey are specified, seleniumServerJar will be ignored.
   // The tests will be run remotely using SauceLabs.
-  sauceUser: null,
-  sauceKey: null,
+  config.sauceUser = null;
+  config.sauceKey = null;
 
   // The address of a running selenium server. If specified, Protractor will
   // connect to an already running instance of selenium. This usually looks like
   // seleniumAddress: 'http://localhost:4444/wd/hub'
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  config.seleniumAddress = 'http://localhost:4444/wd/hub';
 
   // The timeout for each script run on the browser. This should be longer
   // than the maximum time your application needs to stabilize between tasks.
-  allScriptsTimeout: 120000,
+  config.allScriptsTimeout = 120000;
 }
+
 exports.config = config;
